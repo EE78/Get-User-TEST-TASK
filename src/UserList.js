ffab1f7a3593395a6fa3ed251334713dcd5031e1
EE78 <ereminevgeny78@gmail.com>
2022-01-14 21:15:12 +0700

    Add statuses

--- src/UserList.js
+++ src/UserList.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const UserList = () => {
   const [users, setUsers] = useState([]);
   const [isFetchDisabled, setFetchDisabled] = useState(false);
+  const [error, setError] = useState(null);
 
   const onGetUserClick = () => {
     if (isFetchDisabled) return;
@@ -12,6 +13,9 @@ const UserList = () => {
       .then((res) => res.json())
       .then((result) => {
         setUsers(result.filter((user) => user.username !== "Delphine"));
+      })
+      .catch((error) => {
+        setError(error);
       });
   };
 
@@ -36,12 +40,16 @@ const UserList = () => {
         Get users
       </button>
       <div>
-        {users.map((user) => (
-          <div key={user.id}>
-            {user.name}
-            <button onClick={() => handleClick(user)}>LIKE</button>
-          </div>
-        ))}
+        {users.length !== 0
+          ? users.map((user) => (
+              <div key={user.id}>
+                {user.name}
+                <button onClick={() => handleClick(user)}>LIKE</button>
+              </div>
+            ))
+          : "No data"}
+
+        {error && "ERROR"}
       </div>
     </div>
   );
